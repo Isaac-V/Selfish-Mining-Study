6f498dbfb1679280b36121b25a17de3c737678c3
Isaac-V <isaac.vawter@gmail.com>
2016-04-19 09:29:36 -0400

    biteasy web scraper

--- StatPuller.py
+++ StatPuller.py
@@ -3,8 +3,15 @@ import json
 
 url = 'api.biteasy.com'
 connection = http.client.HTTPSConnection(url, 443)
-connection.request('GET', '/v2/btc/mainnet/blocks?type=ORPHANED&per_page=20')
+connection.request('GET', '/v2/btc/mainnet/blocks?page=1&per_page=200')
 response = connection.getresponse()
 source = response.read().decode('utf-8')
 data = json.loads(source)
-print(data)
+print(data['data']['pagination'])
+print(data['data']['blocks'][0])
+bTotal = len(data['data']['blocks'])
+
+for bIndex in range(bTotal):
+    print(data['data']['blocks'][bIndex]['solved_at'])
+    
+print(bTotal)
