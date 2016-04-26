600d62116c8d2268ac701873aec09e774b6f64fc
Isaac-V <isaac.vawter@gmail.com>
2016-04-26 16:36:51 -0400

    Probability functions

--- sequences.py
+++ sequences.py
@@ -26,13 +26,42 @@ def seqs(list):
         
     return seqArray
 
-def 
+def theoryProbs(mainBlks, poolBlks):
+    hPow = []
+    tProbs = []
+    
+    for i in range(len(mainBlks)):
+        hPow.append(len(poolBlks[i])/len(mainBlks[i]))
+        
+    for i in range(len(hPow)):
+        tProbs.append([])
+        for j in range(11):
+            tProbs[i].append(hPow[i]**j)
+        
+    return tProbs
+
+def obsProbs(mainBlks, poolBlks):
+    
+    oProbs = []
+    
+    for i in range(len(poolBlks)):
+        seqVals = seqs(poolBlks[i])
+        oProbs.append([])
+        for j in range(len(seqVals)):
+            oProbs[i].append(seqVals[j]/(len(mainBlks[0]) - 2))
+    
+    return oProbs
+        
           
 def main():
     
-    data = [1,2,3,5,7,8,10]
+    poolBlks = [[1,2,3,5,7,8,10,13,18],[23,25,28,29,30,35,36,39]]
+    mainBlks = [[1,2,3,5,7,8,9,10,11,12,13,14,15,16,17,18,19,20],[21,22,23,23,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]]
     
-    print(seqs(data))
+    oProbs = obsProbs(mainBlks, poolBlks)
+    tProbs = theoryProbs(mainBlks, poolBlks)
     
+    for i in range(len(oProbs)):
+        print(oProbs[i], tProbs[i])
     
 main()
