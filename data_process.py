420ef8edfc6615d6f0af4b8c7a3056c02607c7d0
Kly4 <ashablygin@umass.edu>
2016-04-26 16:34:23 -0400

    block_heights

--- data_process.py
+++ data_process.py
@@ -26,38 +26,39 @@ def main():
         reader = csv.reader(f)
         for row in reader:
             F2pool.append(row)
+    F2pool.pop(0)
+    for i in F2pool:
+        i[0] = int(i[0], 10)
+    F2pool.reverse()
+
+
+
 
     Antpool.pop(0)
     for i in Antpool:
-        i.pop(2)
-        i.pop(2)
-        i.pop(2)
-        i.pop(2)
-        if len(i) == 3:
-            i.pop(2)
-        i[1] = (i[1].split(' ')[0])
-        antDate = i[1][1:]+'/'+i[1][:4]
-        AntpoolTotal.append(andDate.replace('-','/'))
+
+        while len(i) > 1:
+            i.pop(1)
+        i[0] = int(i[0], 10)
+            
+        
 
 
     data.pop(0)
+    for i in data[:]:
+        while len(i) > 1:
+            i.pop(1)
+        if len(i[0]) > 6:
+            data.remove(i)
+
     for i in data:
-        i.pop(2)
-        i.pop(2)
-        i[1] = (i[1].split(' ')[0])
-        dates.append(i[1])
-    
+        i[0] = int(i[0], 10)
+    data.reverse()
 
-    antMap = OrderedDict.fromkeys(AntpoolTotal)
+    print 'F2pool', F2pool[0]   
+    print 'Data', data[0]
+    print 'Antpool', Antpool[0]
 
-    for i in AntpoolTotal:
-        antMap[i] = []
-
-    for i in Antpool:
-        antMap[i[1]].append(i[0])
-      
-    dateMap = OrderedDict.fromkeys(dates)
-    print dateMap
 
         
 main()
