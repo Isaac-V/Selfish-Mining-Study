af428c434c10478e3dc0329e78a6a48d5f8a1564
Kly4 <ashablygin@umass.edu>
2016-04-26 13:51:31 -0400

    BLOCK COUNT

--- data_process.py
+++ data_process.py
@@ -3,60 +3,59 @@ from collections import OrderedDict
 
 
 def main():
-    Antpool = []
-    F2pool = []
-    data = []
-    dates = []
-    totals = []
-    with open('blockchain.csv', 'r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            data.append(row)
+	prev_line = []
+	seq = []
+	Antpool = []
+	F2pool = []
+	data = []
+	dates = []
+	totals = []
+	with open('blockchain.csv', 'rb') as f:
+	    reader = csv.reader(f)
+	    for row in reader:
+	        data.append(row)
 
 
-    with open('AntPoolBlockData.csv', 'r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            Antpool.append(row)
+	with open('AntPoolBlockData.csv', 'rb') as f:
+	    reader = csv.reader(f)
+	    for row in reader:
+	        Antpool.append(row)
 
-    with open('F2PoolBlockData.csv', 'r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            F2pool.append(row)
+	with open('F2PoolBlockData.csv', 'rb') as f:
+	    reader = csv.reader(f)
+	    for row in reader:
+	        F2pool.append(row)
 
-    Antpool.pop(0)
-    for i in Antpool:
-        if len(i[0]) > 6:
-                Antpool.remove(i)
-        else:
-            i[1] = (i[1].split(' ')[0])
-            i[0] = int(i[0], 10)
-            while len(i) > 2:
-                i.pop(2)        
-    
-    cleanedBlks = []
-    data.pop(0)
-    for i in data:
-        if len(i[0]) > 6 or i[0] == 'Height':
-            data.remove(i)
-        else:
-            i[1] = (i[1].split(' ')[0])
-            i[0] = int(i[0], 10)
-            dates.append(i[1])
-            while len(i) > 2:
-                i.pop(2)
-            cleanedBlks.append(i)
-            
-    blkDates = list(OrderedDict.fromkeys(dates))
-    
-    blkTotals = OrderedDict.fromkeys(dates)
-    for i in blkDates:
-        blkTotals[i] = 0
-    
-    for i in cleanedBlks:
-        blkTotals[i[1]] += 1
+	Antpool.pop(0)
+	for i in Antpool:
+		i.pop(2)
+		i.pop(2)
+		i.pop(2)
+		i.pop(2)
+		if len(i) == 3:
+			i.pop(2)
+		i[0] = int(i[0], 10)
+		i[1] = (i[1].split(' ')[0])
 
-    print(blkTotals)
+	data.pop(0)
+	for i in data:
+		i.pop(2)
+		i.pop(2)
+		i[1] = (i[1].split(' ')[0])
+		dates.append(i[1])
+	
+	temp = Counter(dates)
+	# print 'SUM', sum(temp.values())
+	temp = temp.items()
+
+	# for word in temp:
+	# 	print word
+
+	for line in Antpool:
+		if len(prev_line) > 0:
+			print 'PREV LINE', prev_line
+		prev_line = line
+
 
 		
 main()
