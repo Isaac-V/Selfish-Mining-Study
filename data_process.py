251c83fa9b1eabb6127944522f28b517cfc8e83e
Kly4 <ashablygin@umass.edu>
2016-04-26 12:45:00 -0400

    some extra stuff

--- data_process.py
+++ data_process.py
@@ -3,6 +3,8 @@ from collections import OrderedDict
 
 
 def main():
+	Antpool = []
+	F2pool = []
 	data = []
 	dates = []
 	totals = []
@@ -10,6 +12,29 @@ def main():
 	    reader = csv.reader(f)
 	    for row in reader:
 	        data.append(row)
+
+
+	with open('AntPoolBlockData.csv', 'rb') as f:
+	    reader = csv.reader(f)
+	    for row in reader:
+	        Antpool.append(row)
+
+	with open('F2PoolBlockData.csv', 'rb') as f:
+	    reader = csv.reader(f)
+	    for row in reader:
+	        F2pool.append(row)
+
+	Antpool.pop(0)
+	for i in Antpool:
+		i.pop(2)
+		i.pop(2)
+		i.pop(2)
+		i.pop(2)
+		if len(i) == 3:
+			i.pop(2)
+		i[0] = int(i[0], 10)
+		i[1] = (i[1].split(' ')[0])
+
 	data.pop(0)
 	for i in data:
 		i.pop(2)
@@ -19,8 +44,7 @@ def main():
 	
 	temp = list(OrderedDict.fromkeys(dates))
 
-	print len(temp)
-
+	print Antpool
 
 		
 main()
