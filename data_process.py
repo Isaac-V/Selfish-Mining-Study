40e13f8d8717609ca4a8f0ec882e79120423c30e
Kly4 <ashablygin@umass.edu>
2016-04-26 14:04:25 -0400

    block count with Counter

--- data_process.py
+++ data_process.py
@@ -1,61 +1,61 @@
 import csv
-from collections import OrderedDict
+from collections import OrderedDict, Counter
 
 
 def main():
-	prev_line = []
-	seq = []
-	Antpool = []
-	F2pool = []
-	data = []
-	dates = []
-	totals = []
-	with open('blockchain.csv', 'rb') as f:
-	    reader = csv.reader(f)
-	    for row in reader:
-	        data.append(row)
+    prev_line = []
+    seq = []
+    Antpool = []
+    F2pool = []
+    data = []
+    dates = []
+    totals = []
+    with open('blockchain.csv', 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            data.append(row)
 
 
-	with open('AntPoolBlockData.csv', 'rb') as f:
-	    reader = csv.reader(f)
-	    for row in reader:
-	        Antpool.append(row)
+    with open('AntPoolBlockData.csv', 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            Antpool.append(row)
 
-	with open('F2PoolBlockData.csv', 'rb') as f:
-	    reader = csv.reader(f)
-	    for row in reader:
-	        F2pool.append(row)
+    with open('F2PoolBlockData.csv', 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            F2pool.append(row)
 
-	Antpool.pop(0)
-	for i in Antpool:
-		i.pop(2)
-		i.pop(2)
-		i.pop(2)
-		i.pop(2)
-		if len(i) == 3:
-			i.pop(2)
-		i[0] = int(i[0], 10)
-		i[1] = (i[1].split(' ')[0])
+    Antpool.pop(0)
+    for i in Antpool:
+        i.pop(2)
+        i.pop(2)
+        i.pop(2)
+        i.pop(2)
+        if len(i) == 3:
+            i.pop(2)
+        i[0] = int(i[0], 10)
+        i[1] = (i[1].split(' ')[0])
 
-	data.pop(0)
-	for i in data:
-		i.pop(2)
-		i.pop(2)
-		i[1] = (i[1].split(' ')[0])
-		dates.append(i[1])
-	
-	temp = Counter(dates)
-	# print 'SUM', sum(temp.values())
-	temp = temp.items()
+    data.pop(0)
+    for i in data:
+        i.pop(2)
+        i.pop(2)
+        i[1] = (i[1].split(' ')[0])
+        dates.append(i[1])
+    
+    temp = Counter(dates)
+    # print 'SUM', sum(temp.values())
+    temp = temp.items()
 
-	# for word in temp:
-	# 	print word
+    # for word in temp:
+    #   print word
 
-	for line in Antpool:
-		if len(prev_line) > 0:
-			print 'PREV LINE', prev_line
-		prev_line = line
+    for line in Antpool:
+        if len(prev_line) > 0:
+            print 'PREV LINE', prev_line
+        prev_line = line
 
 
-		
+        
 main()
